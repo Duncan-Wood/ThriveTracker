e17888b61541b3f879a5cb92484bda6903af39c4
Duncan <duncanwoodpro@gmail.com>
2023-09-26 17:31:57 -0400

    showed how to edit images in django app

--- New-Django/thrivetracker_app/addiction_calendar/views.py
+++ New-Django/thrivetracker_app/addiction_calendar/views.py
@@ -153,7 +153,7 @@ def update_event(request, event_id):
 
 def update_venue(request, venue_id):
     venue = Venue.objects.get(pk=venue_id)
-    form = VenueForm(request.POST or None, instance=venue)
+    form = VenueForm(request.POST or None, request.FILES or None, instance=venue)
     if form.is_valid():
         form.save()
         return redirect('list-venues')
