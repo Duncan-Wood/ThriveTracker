88bb175e2270d9ee354a0a2991497c91899a7edb
Duncan <duncanwoodpro@gmail.com>
2023-09-26 16:52:26 -0400

    figured out how to filter dates by date

--- New-Django/thrivetracker_app/addiction_calendar/views.py
+++ New-Django/thrivetracker_app/addiction_calendar/views.py
@@ -241,6 +241,12 @@ def home(request, year=datetime.now().year, month=datetime.now().strftime('%B'))
     now = datetime.now()
     current_year = now.year
 
+    # Query the Events model for Dates
+    event_list = Event.objects.filter(
+        event_date__year = year,
+        event_date__month = month_number,
+    )
+
     # get current time
     current_time = now.strftime("%I:%M %p")
 
@@ -252,4 +258,5 @@ def home(request, year=datetime.now().year, month=datetime.now().strftime('%B'))
         'cal': cal,
         'current_year': current_year,
         'current_time': current_time,
+        'event_list': event_list,
         })
